e4b20480dfba3de580795a9fe01b918932c46b02
dutianze <dutianzer@gmail.com>
2022-12-16 23:45:43 +0800

    a

---
+++ vue/vue-test/03_src_props配置/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Student name="李四" sex="女" :age="18" />
+    <Student name="王五" sex="男" :age="18" />
+  </div>
+</template>
+
+<script>
+import Student from "./components/Student";
+
+export default {
+  name: "App",
+  components: { Student },
+};
+</script>
