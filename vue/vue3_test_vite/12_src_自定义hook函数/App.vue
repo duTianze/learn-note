3517767847c1927b1afa87e7df5bfabd17cbc681
dutianze <dutianzer@gmail.com>
2023-06-22 09:01:54 +0900

    refs

---
+++ vue/vue3_test_vite/12_src_自定义hook函数/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <!--vue3的组件模版结构可以没有根标签-->
+  <Demo />
+</template>
+
+<script>
+import Demo from "./components/Demo.vue";
+export default {
+  name: "App",
+  components: { Demo },
+  setup() {},
+};
+</script>
